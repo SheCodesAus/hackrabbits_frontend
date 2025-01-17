6e43db549c714f200223859df7c75befb38cbb40
Behnaz <behnaz.shojaei88@gmail.com>
2025-01-17 22:51:58 +0800

    Tested slideshow functionality
    - Images optimized for smaller volume to ensure fast upload
    - Applied basic CSS styling
    - Final CSS adjustments are pending

--- src/components/BackgroundSlideshow.jsx
+++ src/components/BackgroundSlideshow.jsx
@@ -4,6 +4,9 @@ import "./BackgroundSlideshow.css";
 
 const images = [
     // links of images
+    "https://shecodes.com.au/wp-content/uploads/2021/10/She-Codes_-220-e1639540932458.jpg",
+    "https://women-in-tech-mentoring.oneuponedown.org/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmain-img-wit-min.d91c76f2.jpg&w=3840&q=75",
+    "https://shecodes.com.au/wp-content/uploads/2021/10/pssmileproduction_shecodes-59-scaled.jpg"
 ];
 
 const BackgroundSlideshow = () => {
