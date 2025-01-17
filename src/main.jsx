ac5b747601a7de7995c8beb348843427a3d4c643
Behnaz <behnaz.shojaei88@gmail.com>
2025-01-17 21:28:50 +0800

    remove unnecessary comments

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import HomePage from "./pages/HomePage.jsx";
 
 
 const router = createBrowserRouter([
-  // These are the three routes!
   {
     path: "/",
     element: <HomePage />,
@@ -17,7 +16,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* Here we wrap our app in the router provider so the pages render */}
     <RouterProvider router={router} />
   </React.StrictMode>
 );
