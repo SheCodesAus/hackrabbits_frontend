ee133dbbc5b0e737469a76caaec2c01da5eadae3
Behnaz <behnaz.shojaei88@gmail.com>
2025-01-17 20:57:55 +0800

    remove the error handling to keep for now. Will reapply it later

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,17 @@
+
 import "./HomePage.css";
 import BackgroundSlideshow from "../components/BackgroundSlideshow";
 
 function HomePage() {
-    const { rolemodels, isLoading, error } = useRolemodels();
-    // will use the api for get rolemodel cards here 
 
-    if (isLoading) {
-        return (<p>loading...</p>);
-    }
 
-    if (error) {
-        return (<p>Error: {error.message}</p>);
-    }
+    // if (isLoading) {
+    //     return (<p>loading...</p>);
+    // }
+
+    // if (error) {
+    //     return (<p>Error: {error.message}</p>);
+    // }
 
 
     return (
@@ -24,8 +24,8 @@ function HomePage() {
             <div id="app" className="featured_rolemodels">
                 <div className="sheinspires_header">
                     <div className="sheinspires_header_description">
-                        <h1>She Inspires!</h1>
-                        <p>nice content about website in 1-2 sentence</p>
+                        {/* <h1>She Inspires!</h1>
+                        <p>nice content about website in 1-2 sentence</p> */}
                     </div>
                 </div>
             </div>
