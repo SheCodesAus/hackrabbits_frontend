0b543c49f990427a754d8e61d58800a450612ef8
Sicilia <sicilia.perumalsamy@research.uwa.edu.au>
2025-01-19 22:57:26 +0800

    fixed missing code for homepage slideshow

--- src/main.jsx
+++ src/main.jsx
@@ -4,25 +4,18 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/HomePage.jsx";
-import NavBar from "./components/NavBar.jsx";
 
-import { AuthProvider } from "./components/AuthProvider.jsx";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <NavBar />,
-    children: [
-      { path: "/", element: <HomePage /> }
-    ],
-  },
+    element: <HomePage />,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
