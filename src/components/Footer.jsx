6df589bb87af41818d695e0b4f29f9f92e2fc4dc
Katie <katie.connell.91@gmail.com>
2025-01-25 15:18:55 +0800

    formatted footer MK2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,26 +3,27 @@ import "../styles.css";
 
 function Footer() {
   return (
-    <div className="iconcontainer">
-      <img
-        className="inclusiveicons"
-        src="img/femalesymbol.png"
-        alt="the symbol for female"
-      />
-
-      <img
-        className="inclusiveicons"
-        src="img/nonbinarysymbol.jpg"
-        alt="the symbol for non-binary"
-      />
-
-      <img
-        className="inclusiveicons"
-        src="img/uniteicon.png"
-        alt="diverse colours of hands surrounding a heart"
-      />
-
-      <p className="copyright">© HackRabbits 2025</p>
+    <div className="footer">
+      <div className="iconcontainer">
+        <img
+          className="inclusiveicons"
+          src="img/femalesymbol.png"
+          alt="the symbol for female"
+        />
+        <img
+          className="inclusiveicons"
+          src="img/nonbinarysymbol.jpg"
+          alt="the symbol for non-binary"
+        />
+        <img
+          className="inclusiveicons"
+          src="img/uniteicon.png"
+          alt="diverse colours of hands surrounding a heart"
+        />
+      </div>
+      <div className="copyright">
+        <p>© HackRabbits 2025</p>
+      </div>
     </div>
   );
 }
