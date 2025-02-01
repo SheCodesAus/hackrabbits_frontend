9e0bec899420a52d73056c0ecbe7e3a9150e8c24
Behnaz <behnaz.shojaei88@gmail.com>
2025-02-01 19:25:48 +0800

    toggle for contact added, touch up the page

--- src/components/RMProfileDetails.jsx
+++ src/components/RMProfileDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import fetchLimitedRoleModelProfile from "../api/rolemodeluser_profile/get_publicview_profile";
 import fetchRolemodel from "../api/rolemodeluser_profile/get_fulldetailsprofile";
 import { useNavigate } from "react-router-dom";
-
+import ContactForm from "./ContactForm";
 
 // BS. I need to get id using the useparam and hook now I wonder if it's matter to get the id from which endpoint? I don't think so! 
 
@@ -34,6 +34,17 @@ const RoleModelProfileDetails = ({ rolemodelId }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+  const [showContactForm, setShowContactForm] = useState(false);
+
+  // toggle for contact form 
+  const handleContactRequest = () => {
+    setShowContactForm(!showContactForm);
+  };
+
+  // Toggle for Contact Form
+  // const handleContactRequest = () => {
+  //   setShowContactForm((prev) => !prev);
+  // };
 
   useEffect(() => {
     // Check authentication
@@ -71,8 +82,6 @@ const RoleModelProfileDetails = ({ rolemodelId }) => {
       <p><strong>Location:</strong> {profile.location}</p>
 
 
-
-
       {isAuthenticated ? (
         <>
           <p><strong>Skills:</strong> {profile.skills?.join(", ")}</p>
@@ -85,14 +94,37 @@ const RoleModelProfileDetails = ({ rolemodelId }) => {
 
           <h3>Advice</h3>
           <p>{profile.advice || " "}</p>
+
+          {/* Contact Form */}
+          <button onClick={handleContactRequest} className="button">
+            Contact
+          </button>
+
+          {/* I am tryin to implement a contact form button happen.
+               set up a toggle and the button here. 
+               but not sure how to handle when i call contact form. 
+               this pieace of code is borrowed from my other project  */}
+
+          {/* Contact Form */}
+          {showContactForm && (
+            <div className="contact-form-container">
+              <ContactForm onContactSubmitted={refetch} />
+              <button onClick={handleContactRequest} className="button">
+                Cancel
+              </button>
+            </div>
+          )}
+          {/* need to pass onContactSubmitted to contact form */}
         </>
       ) : (
         <button onClick={() => navigate("/login")}>
+          {/* hook to save the profile page so after login we come back to the same page */}
           Learn More
-        </button>
-      )}
+        </button>)
+      }
     </div>
-  );
-};
+  )
+}
+
 
 export default RoleModelProfileDetails;
